625bcd725b8ee2d783912b711d2d9af5e7e6ed5a
Carra Hamner <carrah@usc.edu>
2023-05-04 16:39:20 -0700

    file name changes

--- main.py
+++ main.py
@@ -16,7 +16,16 @@ else:
 path_scene = "C:/Users/genac/PycharmProjects/pythonProject6/output/"
 
 
-def result_generator(directory_path, input_file, threshold):
+def file_name_generator(level):
+    if level == 0:
+        return "$VIDEO_NAME - Scene $SCENE_NUMBER.mp4"
+    elif level == 1:
+        return "$VIDEO_NAME - Shot $SCENE_NUMBER.mp4"
+    elif level == 2:
+        return "$VIDEO_NAME - Subshot $SCENE_NUMBER.mp4"
+
+
+def result_generator(directory_path, input_file, threshold, level):
     # if the directory exists ,delete it and all contents
     try:
         shutil.rmtree(directory_path)
@@ -28,7 +37,7 @@ def result_generator(directory_path, input_file, threshold):
     # Check if the directory was created successfully.
     if not os.path.isdir(directory_path):
         print(f"Directory '{directory_path}' created successfully.")
-    output_directory = os.path.join(directory_path, "$VIDEO_NAME - Scene $SCENE_NUMBER.mp4")
+    output_directory = os.path.join(directory_path, file_name_generator(level))
     detector = scenedetect.detectors.content_detector.ContentDetector(threshold=threshold)
     scene_list = scenedetect.detect(input_file, detector, stats_file_path=os.path.join(directory_path, "result.csv"))
     split_video_ffmpeg(
@@ -39,7 +48,7 @@ def result_generator(directory_path, input_file, threshold):
     )
 
 
-result_generator(path_scene, input_path, 63)
+result_generator(path_scene, input_path, 63, 0)
 
 for filename in enumerate(os.listdir(path_scene)):
     if filename[1].endswith(".mp4"):
@@ -49,16 +58,11 @@ for filename in enumerate(os.listdir(path_scene)):
             # Perform any operation on the file
             print(f"Processing file {path_shot}")
             path_shot_result = os.path.join(path_scene, str(filename[0] + 1)) + "/"
-            result_generator(path_shot_result, path_shot, 27)
+            result_generator(path_shot_result, path_shot, 27, 1)
             for file_name in enumerate(os.listdir(path_shot_result)):
                 if file_name[1].endswith(".mp4"):
                     path_sub_shot = os.path.join(path_shot_result, file_name[1])
                     if os.path.isfile(path_sub_shot):
                         print(f"Processing file {path_sub_shot}")
                         path_sub_shot_result = os.path.join(path_shot_result, str(file_name[0] + 1)) + "/"
-                        result_generator(path_sub_shot_result, path_sub_shot, 14)
-
-
-
-
-
+                        result_generator(path_sub_shot_result, path_sub_shot, 14, 2)
